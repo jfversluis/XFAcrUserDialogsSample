d7f9da618643b7c34de203289369b2f908f0e3cc
Gerald Versluis <gerald@verslu.is>
2021-10-11 13:06:21 +0200

    Implement UserDialogs

--- XFAcrUserDialogsSample/MainPage.xaml.cs
+++ XFAcrUserDialogsSample/MainPage.xaml.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Acr.UserDialogs;
 using Xamarin.Forms;
 
 namespace XFAcrUserDialogsSample
@@ -13,6 +8,51 @@ namespace XFAcrUserDialogsSample
         public MainPage()
         {
             InitializeComponent();
+        }
+
+        void Button_Clicked(System.Object sender, System.EventArgs e)
+        {
+            UserDialogs.Instance.Alert(new AlertConfig
+            {
+                Message = "You're now subscribed to Gerald's channel",
+                OkText = "OK, thank you",
+                Title = "Subscribed",
+                OnAction = () => { UserDialogs.Instance.Alert("Dismissed"); }
+            });
+        }
+
+        void Button_Clicked_1(System.Object sender, System.EventArgs e)
+        {
+            UserDialogs.Instance.Confirm(new ConfirmConfig
+            {
+                Message = "Do you want to subscribe to Gerald's channel?",
+                OkText = "Yes, please",
+                CancelText = "No, thank you",
+                Title = "Subscribe?",
+                OnAction = (confirmed) => {
+                    if (confirmed)
+                        UserDialogs.Instance.Alert("Dismissed");
+                },
+            });
+        }
+
+        void Button_Clicked_2(System.Object sender, System.EventArgs e)
+        {
+            UserDialogs.Instance.Prompt(new PromptConfig
+            {
+                Message = "Please input something",
+                CancelText = "Cancel",
+                Placeholder = "Input text here",
+                Title = "This is a prompt"
+            });
+        }
+
+        void Button_Clicked_3(System.Object sender, System.EventArgs e)
+        {
+            UserDialogs.Instance.Loading("Loading data...", () =>
+            {
+                UserDialogs.Instance.HideLoading();
+            }, "Cancel", true, MaskType.Gradient);
         }
     }
 }
